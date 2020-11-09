e823f5ba9b81d464a8757cfbcc81947f9bbbe1c0
Zichen <changzichen2000@gmail.com>
2020-11-09 08:51:06 -0500

    Refactor Team number

--- controllers/T02Controller/T02Controller.java
+++ controllers/T02Controller/T02Controller.java
@@ -1,7 +1,7 @@
 /**
  * Wrapper class for webots to start the main project method.
  */
-public class TXXController {
+public class T02Controller {
   public static void main(String[] args) {
     ca.mcgill.ecse211.project.Main.main(args);
   }
